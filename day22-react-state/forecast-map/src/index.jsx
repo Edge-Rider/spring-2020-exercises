7469eb086ddc3d4dc38e482ad6654c5817ccb86e
Martin Podloucký <podlouckymartin@gmail.com>
2020-05-26 12:00:39 +0200

    Forecast with maps.

---
+++ day22-react-state/forecast-map/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DayForecast from "./DayForecast/index.jsx";
+import { forecast } from "./forecast.js";
+import "./index.html";
+import "./style.css";
+
+class WeeklyForecast extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Weekly Weather Forecast</h1>
+        <div class="week-forecast">
+          {forecast.map((day) => (
+            <DayForecast
+              weekday={day.weekday}
+              weather={day.weather}
+              tempDay={day.tempDay}
+              tempNight={day.tempNight}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<WeeklyForecast />, document.getElementById("app"));
