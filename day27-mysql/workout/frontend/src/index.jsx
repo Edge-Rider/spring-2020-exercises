a4014e6804e377db5387151d50a93d20fc320896
j_polak <jan.polak@onmyway.cz>
2020-06-02 10:36:48 +0200

    day 27 workout done

--- day27-mysql/workout/frontend/src/index.jsx
+++ day27-mysql/workout/frontend/src/index.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <>
         <Celebrities 
-          url=""
+          url="http://www.cbp-exercises.test/day27-mysql/workout/backend/"
         />
       </>
     );
