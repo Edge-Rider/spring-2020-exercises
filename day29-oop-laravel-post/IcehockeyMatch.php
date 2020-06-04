b87aa0f6cd3684d9f0b3344616ca027e346d67e9
j_polak <jan.polak@onmyway.cz>
2020-06-04 14:05:40 +0200

    day 29 inheritance & visibility:

---
+++ day29-oop-laravel-post/IcehockeyMatch.php
@@ -0,0 +1,17 @@
+<?php
+
+class IcehockeyMatch extends Match
+{
+    public $nr_of_players = 12;
+    public $length = 60;
+    public $thirds = [
+        '0:0', // 0
+        '0:0', // 1
+        '0:0'  // 2
+    ];
+
+    public function getThirdScore($third_nr)
+    {
+        return $this->thirds[$third_nr - 1];
+    }
+}
