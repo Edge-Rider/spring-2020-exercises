bde909215dce92a3903e6b654ce7e7b081cad52f
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2020-06-23 10:13:37 +0200

    fix

--- day42-react-hooks/morning-workout-sate-props/src/Components/Display.jsx
+++ day42-react-hooks/morning-workout-sate-props/src/Components/Display.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Display extends Component {
   render() {
     const { count } = this.props;
-    return <h2>{this.props.count}</h2>;
+    return <h2>{count}</h2>;
   }
 }
 
