388b0aa2211239b6320e300226aa1e2319a1406b
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2020-06-24 10:58:37 +0200

    Link to search results

--- day43-react-functional/morning-workout-starter/src/components/SearchResults.jsx
+++ day43-react-functional/morning-workout-starter/src/components/SearchResults.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
+const wikiBaseUrl = "https://en.wikipedia.org/wiki/";
+
 class SearchResults extends Component {
   render() {
     const { searchResults } = this.props;
     return (
       <div className="searchResults-container">
         {searchResults.map((result, index) => (
-          <div key={index}>
-            <h2>{result.title}</h2>
-            <p>{result.pageid}</p>
-          </div>
+          <h2 key={index}>
+            <a
+              href={`${wikiBaseUrl}${result.title}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {result.title}
+            </a>
+          </h2>
         ))}
       </div>
     );
