db7b85473aeeddf682485af8b475bdca650ee2f6
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2020-06-25 17:24:02 +0200

    countries rest API

--- day44-react-router/react-recap-challenge/webpack.config.js
+++ day44-react-router/react-recap-challenge/webpack.config.js
@@ -1,58 +1,55 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/js/index.jsx",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-          ],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
           plugins: [
-            '@babel/plugin-proposal-class-properties',
-            'babel-plugin-transform-async-to-promises',
+            "@babel/plugin-proposal-class-properties",
+            "babel-plugin-transform-async-to-promises",
           ],
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name]-[hash:6].[ext]',
-              outputPath: 'img'
-            }
-          }
-        ]
+              name: "[name]-[hash:6].[ext]",
+              outputPath: "img",
+            },
+          },
+        ],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
