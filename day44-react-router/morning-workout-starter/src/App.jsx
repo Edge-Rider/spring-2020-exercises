67ff5f0ed21e2ffe72ea53454a692dd77d0eab7e
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2020-06-25 09:21:40 +0200

    starter fixes

--- day44-react-router/morning-workout-starter/src/App.jsx
+++ day44-react-router/morning-workout-starter/src/App.jsx
@@ -1,58 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar'
-import SearchResults from './components/SearchResults'
+import React from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
 
-const url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch='
-const pageOffsetPrefix = '&sroffset='
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=";
+const pageOffsetPrefix = "&sroffset=";
 
+const App = () => {
+  const handleInputChange = (e) => {};
 
-function App () {
-  const [ searchValue, setSearchValue ] = useState('')
-  const [ pageOffset, setPageOffset ] = useState(0)
-  const [ searchResults, setSearchResults ] = useState([])
+  const handleSearchClick = () => {};
 
-  const handleInputChange = (e) => {
-    setSearchValue(e.target.value)
-  }
+  const handleKeyPress = (e) => {};
 
-  const handleSearchClick = () => {
-    getSearchResults()
-  }
-
-  const handleKeyPress = (e) => {
-    if(e.key === 'Enter') getSearchResults()
-  }
-
-  const getSearchResults = async () => {
-    if(!searchValue) return
-    console.log('Getting Search Results')
-    const searchUrl = `${url}${searchValue}${pageOffsetPrefix}${pageOffset}`
-    console.log('url', searchUrl)
-    const response = await fetch(searchUrl)
-    const data = await response.json()
-    console.log('data', data)
-    setSearchResults(data.query.search)
-  }
-
-  const handleNextPageClick = (event) => {
-    setPageOffset(pageOffset + 10)
-  }
-
-  useEffect(() => {
-    getSearchResults()
-  }, [pageOffset]) // Empty array is thes same as componentDidMount
   return (
     <div className="App">
       <SearchBar
-        searchValue={searchValue}
         handleInputChange={handleInputChange}
         handleSearchClick={handleSearchClick}
         handleKeyPress={handleKeyPress}
       />
-      <SearchResults searchResults={searchResults} handleNextPageClick={handleNextPageClick}/>
-  </div>
+      <SearchResults />
+    </div>
   );
-}
+};
 
 export default App;
--- day44-react-router/morning-workout-starter/src/components/SearchResults.jsx
+++ day44-react-router/morning-workout-starter/src/components/SearchResults.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 
-const SearchResults = ({ searchResults, handleNextPageClick }) => {
-  const articleLinks = searchResults.map(giveMeMyLinks);
+const SearchResults = () => {
   return (
-    <div className="searchResults-container">
-      Add search results here.
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        {articleLinks}
-      </div>
-      <button onClick={handleNextPageClick}>Next Page</button>
-    </div>
+    <div className="searchResults-container">Add search results here.</div>
   );
 };
 
 export default SearchResults;
-
-function giveMeMyLinks(result) {
-  return (
-    <a key={result.title} href={"/"}>
-      {result.title}
-    </a>
-  );
-}
