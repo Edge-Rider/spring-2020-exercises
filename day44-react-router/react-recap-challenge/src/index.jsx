e809e953992105abefb22c155dd6760c94baefc4
Daniela Zelinková <daniela.zelinkova@rohlik.cz>
2020-06-25 15:36:05 +0200

    fix of the challenge

--- day44-react-router/react-recap-challenge/src/index.jsx
+++ day44-react-router/react-recap-challenge/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
+import App from "./App.jsx";
+import "./index.html";
 import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
